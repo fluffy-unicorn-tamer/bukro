00bf9c5d5453eb910a17e1017364daf1e27e17f1
fluffy-unicorn-dev <luan.oelsner@outlook.de>
2023-03-08 17:44:40 +0100

    added search by name & nachname + delete

--- src/Mitglied/mitglied.controller.ts
+++ src/Mitglied/mitglied.controller.ts
@@ -2,25 +2,35 @@
 https://docs.nestjs.com/controllers#controllers
 */
 
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Body, Param } from '@nestjs/common';
 import { MitgliedService } from './mitglied.service';
-  import { Mitglied } from './mitglied';
-
-
+import { Mitglied } from './mitglied';
 
 @Controller('mitglieder')
 export class MitgliedController {
-
-
   constructor(private readonly mitgliedService: MitgliedService) {}
   @Get()
-  getAllMitglieder(){
+  getAllMitglieder() {
     return this.mitgliedService.getAllMitglieder();
   }
 
+  @Get('name')
+  getMitgliedByName(@Body() body: any) {
+    return this.mitgliedService.getMitgliedByName(body);
+  }
+
+  @Get('nachname')
+  getMitgliedByNachname(@Body() body: any) {
+    return this.mitgliedService.getMitgliedByNachname(body);
+  }
 
   @Post()
-  addNewMitglied(@Body()  body: Mitglied) {
+  addNewMitglied(@Body() body: Mitglied) {
     return this.mitgliedService.addNewMitglied(body);
+  }
+
+  @Delete(':id')
+  deleteMitgliedById(@Param() params) {
+    return this.mitgliedService.deleteMitgliedById(params);
   }
 }
--- src/Mitglied/mitglied.service.ts
+++ src/Mitglied/mitglied.service.ts
@@ -4,6 +4,7 @@ https://docs.nestjs.com/providers#services
 
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { response } from 'express';
 import { Mitglied } from './mitglied';
 const prisma = new PrismaClient();
 
@@ -11,6 +12,24 @@ const prisma = new PrismaClient();
 export class MitgliedService {
   async getAllMitglieder() {
     const respond = await prisma.mitglieder.findMany();
+    return respond;
+  }
+
+  async getMitgliedByName(body: any) {
+    const respond = await prisma.mitglieder.findMany({
+      where: {
+        name: body.name,
+      },
+    });
+    return respond;
+  }
+
+  async getMitgliedByNachname(body: any) {
+    const respond = await prisma.mitglieder.findMany({
+      where: {
+        nachname: body.nachname,
+      },
+    });
     return respond;
   }
 
@@ -24,6 +43,15 @@ export class MitgliedService {
         iban: body.iban,
         hausnummer: body.hausnummer,
         strasse: body.strasse,
+      },
+    });
+    return respond;
+  }
+
+  async deleteMitgliedById(params: any) {
+    const respond = await prisma.mitglieder.delete({
+      where: {
+        id: params.id,
       },
     });
     return respond;
